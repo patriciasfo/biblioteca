c4515be4f2721e8fe3d70da9de8972447aab487e
patriciasfo <patriciasfo@educastur.org>
2025-10-31 20:16:02 +0100

    cambio en el repositorio

--- src/main/java/com/example/biblioteca/repository/LibroRepository.java
+++ src/main/java/com/example/biblioteca/repository/LibroRepository.java
@@ -2,8 +2,13 @@ package com.example.biblioteca.repository;
 
 import com.example.biblioteca.entity.Libro;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface LibroRepository extends JpaRepository<Libro, Long> {
+    /*@Query(name = "SELECT l FROM Libro l")
+    List<Libro> listaLibros();*/
 }
